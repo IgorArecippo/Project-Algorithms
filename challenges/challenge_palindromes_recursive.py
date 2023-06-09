e2524d93fa076aea8688e6a2e06068925b8ecf5f
Igor Arecippo <iarecippo@gmail.com>
2023-06-09 15:21:10 -0300

    req 03 recursividade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+def is_palindrome_recursive(word, low_index=0, high_index=None):
+    if high_index is None:
+        high_index = len(word) - 1
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index].lower() != word[high_index].lower():
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
     raise NotImplementedError
