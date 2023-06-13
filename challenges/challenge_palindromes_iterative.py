5faaa1405e6815ebb0cb716348df08249f5075c6
Igor Arecippo <iarecippo@gmail.com>
2023-06-13 09:37:46 -0300

    req palindromos iterativo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if not word:
+        return False
+
+    word = word.lower().replace(" ", "")  # Converter para minúsculas e remover espaços
+    length = len(word)
+
+    for i in range(length // 2):
+        if word[i] != word[length - 1 - i]:
+            return False
+
+    return True
     raise NotImplementedError
